f8dd5ed360e1256edd006f5fb30af141f742d048
Rami Dridi <ramy.dridi@hotmail.com>
2020-01-24 01:43:19 +0100

    idunno

--- src/Component/Scheduler.jsx
+++ src/Component/Scheduler.jsx
@@ -1,14 +1,30 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import axios from 'axios';
 import './User.css'
+import {getDocumentList} from './utils/firestore'
+import MyListItem from "./MyListItem";
 
 
 class Scheduler extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            tags: [],
+            tag : ''
+        }
+    }
+
+    updateTags = async () => {
+
+        this.setState({tags: await getDocumentList('tags')})
+console.log('status')
+console.log(this.state)
+    }
+
+   async componentDidMount() {
+       await this.updateTags()
     }
 
     handleChange(event) {
@@ -21,17 +37,17 @@ class Scheduler extends Component {
     }
 
     handleSubmit(event) {
-        let data = this.state
+        let {tags, ...data} = this.state
         event.preventDefault()
-        console.log('data')
         console.log(data)
         let $this = this
         axios.post(`http://127.0.0.1:5000/run-tasks`,
-            {data}  )
+            {data})
             .then(res => {
-                 console.log(res.data)
+                console.log(res.data)
             })
     }
+
 
     render() {
         return (
@@ -39,37 +55,45 @@ class Scheduler extends Component {
                 <div className='container'>
 
 
-                  <form action="">
-                      <div className="form-group">
-                      <label htmlFor='modal-select'>Enter Year</label>
-                      <input type='text' name="year"  class="form-control" onChange={this.handleChange}></input>
-                      </div>
-                          <div className="form-group">
-                      <label htmlFor='modal-select'>Enter Month</label>
-                      <input type='text' name="month" class="form-control"  onChange={this.handleChange}></input>
-                          </div>
-                              <div className="form-group">
-                      <label htmlFor='modal-select'>Enter Day</label>
-                      <input type='text' name="day"   class="form-control" onChange={this.handleChange}></input>
-                              </div>
-                                  <div className="form-group">
-                      <label htmlFor='modal-select'>Enter Hour</label>
-                      <input type='text' name="hour"  class="form-control" onChange={this.handleChange}></input>
-                                  </div>
-                                      <div className="form-group">
-                      <label htmlFor='modal-select'>Enter Minute</label>
-                      <input type='text' name="minute"   class="form-control" onChange={this.handleChange}></input>
-                                      </div>
-                                          <div className="form-group">
-                      <label htmlFor='modal-select'>Enter URL</label>
-                      <input type='text' name="url"   class="form-control" onChange={this.handleChange}></input>
-                                          </div>
-                    <div className="modal-footer">
-                      <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Save</button>
+                    <form action="">
+                        <div className="form-group">
+                            <label htmlFor='modal-select'>Enter Year</label>
+                            <input type='text' name="year" class="form-control" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor='modal-select'>Enter Month</label>
+                            <input type='text' name="month" class="form-control" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor='modal-select'>Enter Day</label>
+                            <input type='text' name="day" class="form-control" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor='modal-select'>Enter Hour</label>
+                            <input type='text' name="hour" class="form-control" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor='modal-select'>Enter Minute</label>
+                            <input type='text' name="minute" class="form-control" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor='modal-select'>Enter URL</label>
+                            <input type='text' name="url" class="form-control" onChange={this.handleChange}></input>
+                        </div>
+                        <div className="form-group">
 
-                    </div>
+                            <select value={this.state.tag} name='tag' onChange={this.handleChange}>
+                                            {this.state.tags.map((tag, index) =>
+                              <option key={index} value={tag.name}>{tag.name}</option>
+                                                    )}
+                            </select></div>
 
-                  </form>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-primary" onClick={this.handleSubmit}>Save</button>
+
+                        </div>
+
+                    </form>
 
 
                 </div>
